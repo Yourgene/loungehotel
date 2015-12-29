3168386111491e945feb7f51ed94d6956728b499
Yourgene <vladislav.sokolov@hotmail.fr>
2015-12-29 14:28:45 +0100

    z
    zz

--- vue/metaheader.php
+++ vue/metaheader.php
@@ -1,14 +1,5 @@
-<?php 
-	if (isset($_SESSION['username'])) {
-      // This session already exists, should already contain data
-        echo "User ID:", $_SESSION['user_id'], "<br />";
-        echo "User Name:", $_SESSION['username'], "<br />";
-    }	
-?>
-
 <!DOCTYPE HTML>
 <html>
-
 	<head>
 		<meta charset = "utf-8" />
 		<title>Gestion Hotel</title>
@@ -16,13 +7,11 @@
 	</head>
 
 <header>
-<!--Création du bandeaux au dessus de la photo avec le Nom du Groupe -->
-	<div class="Bandeau"><a href="accueil"><img class="iconeSGDF" src="<?php echo CHEMIN_IMAGES ?>LOGO-SGDF.png" alt="logo-sgdf"></a>
-		<ul id="menu1">
-			<li id="liste"><a href="#">Présentation</a></li>
-			<li id="liste"><a href="galerie">Galerie</a></li>
-			<li id="liste"><a href="evenements">Events</a></li>
-			<li id="liste"><a href="#">Contact</a></li>
+<!--Bandeau de navigation -->
+	<div>
+		<ul>
+			<li><a href="#">Nouvel Hebergement</a></li>
+			<li><a href="#">Nouvele Réservation</a></li>
 		</ul>
 	</div>
 </header>
