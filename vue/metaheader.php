254847970d7958de73090004fed490528bd95963
Yourgene <vladislav.sokolov@hotmail.fr>
2016-01-13 16:40:29 +0100

    ez

--- vue/metaheader.php
+++ vue/metaheader.php
@@ -8,8 +8,8 @@
   		<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
   		<!-- <link rel="stylesheet" href="/resources/demos/style.css"> -->
         <base href="http://localhost/loungehotel/">
-        <link href="localhost/loungehotel/css/bootstrap/dist/css/csstest.css" rel="stylesheet">
-        <link href="localhost/loungehotel/css/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+        <link href="//localhost/loungehotel/css/bootstrap/dist/css/csstest.css" rel="stylesheet">
+        <link href="//localhost/loungehotel/css/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
 	</head>
 
 <!--Bandeau de navigation -->
@@ -35,19 +35,7 @@
     </div>
 </nav>
 </div>
-<div class="row">
- <nav class="navbar navbar-inverse">
-        <div class="container-fluid">
-          <ul class="nav navbar-nav">
-            <li class="active"> <a href="#">Accueil</a> </li>
-            <li> <a href="#">Nouvelle Réservation</a> </li>
-            <li> <a href="#">Nouvel Hébergement</a> </li>
-            <li> <a href="#">Connexion</a> </li>
-          </ul>
-        </div>
-      </nav>
 
-</div>
 
 
 <body>
